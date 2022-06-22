9650839a70c42504d9bb99ba500dcff5e47944eb
Brendan Young <brendanyoung@brendans-mbp.lan>
2022-06-22 12:09:20 +1000

    added event listener to board

--- ttt.js
+++ ttt.js
@@ -1,15 +1,42 @@
-const cellBlocks = document.querySelectorAll('cellBlocks')
-const c1 = document.querySelector('.c1')
-const c2 = document.querySelector('.c2')
+const cellBlocks = document.querySelectorAll('.cell')
 
-// cellBlocks.addEventListener('click', () => {
-//     console.log('cells clicked')
-// })
-
-c1.addEventListener('click', () => {
-    console.log('c1 clicked')
-})
-
-function handleClick(e) {
-    console.log('clicked')
+const cellClick = () => {
+    for (let index = 0; index < cellBlocks.length; index++) {
+        cellBlocks[index].addEventListener('click', () => {
+            console.log('clicked')
+        })
+    }
 }
+
+cellClick()
+
+const playerPunchClass = 'x'
+const playerKickClass = 'cirlce'
+const winningCombinations = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+const winningMessage = document.querySelector('.winningMessage')
+const playAgainButton = document.querySelector('.playAgain')
+
+
+// how to swtich turns between X and circle 
+let isPlayerKickTurn = false
+
+const startFight = () => {
+    isPlayerKickTurn = false
+    cellBlocks.classList
+}
+
+
+//how to put token inside of the div 
+
+//try to figure out how to check winning combination and find winner
+
+// showing winning message 
